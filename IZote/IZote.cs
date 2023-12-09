733008edead709b5737965a101482d56d3f9febd
Nearrin Yu <nearrin@outlook.com>
2023-12-09 23:38:41 +0800

    U

--- IZote/IZote.cs
+++ IZote/IZote.cs
@@ -1,5 +1,7 @@
 ﻿global using Modding;
 global using UnityEngine;
+using Satchel;
+
 namespace IZote;
 public class IZote : Mod
 {
@@ -13,26 +15,8 @@ public class IZote : Mod
     }
     public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
     {
-        UnityEngine.SceneManagement.SceneManager.activeSceneChanged += ActiveSceneChanged;
         ModHooks.HeroUpdateHook += HeroUpdateHook;
         greyPrinceTemplate = preloadedObjects["GG_Grey_Prince_Zote"]["Grey Prince"];
-    }
-    private void ActiveSceneChanged(UnityEngine.SceneManagement.Scene from, UnityEngine.SceneManagement.Scene to)
-    {
-        if (HeroController.instance != null)
-        {
-            var knight = HeroController.instance.gameObject;
-            var greyPrinceTransform = knight.transform.Find("Grey Prince");
-            if (greyPrinceTransform == null)
-            {
-                LogDebug("Detected a Knight w/o a Grey Prince. Adding one.");
-                var greyPrince = UnityEngine.Object.Instantiate(greyPrinceTemplate, knight.transform);
-            }
-            else
-            {
-                LogDebug("Detected a Knight with a Grey Prince.");
-            }
-        }
     }
     private void HeroUpdateHook()
     {
@@ -42,8 +26,45 @@ public class IZote : Mod
             var greyPrinceTransform = knight.transform.Find("Grey Prince");
             if (greyPrinceTransform != null)
             {
+                Log("Removing Grey Prince.");
                 var greyPrince = greyPrinceTransform.gameObject;
-                greyPrince.SetActive(!greyPrince.activeSelf);
+                UnityEngine.Object.Destroy(greyPrince);
+            }
+            else
+            {
+                Log("Adding Grey Prince.");
+                var greyPrince = UnityEngine.Object.Instantiate(greyPrinceTemplate, knight.transform);
+                greyPrince.SetActive(true);
+                greyPrince.name = "Grey Prince";
+                greyPrince.transform.localScale = new Vector3(-1, 1, 1);
+                UnityEngine.Object.Destroy(greyPrince.LocateMyFSM("Constrain X"));
+                UnityEngine.Object.Destroy(greyPrince.GetComponent<DamageHero>());
+                UnityEngine.Object.Destroy(greyPrince.GetComponent<Rigidbody2D>());
+                var control = greyPrince.LocateMyFSM("Control");
+                control.RemoveTransition("Init", "FINISHED");
+                control.AddTransition("Init", "FINISHED", "Level 3");
+                control.RemoveAction("Dormant", 3);
+                control.AddCustomAction("Dormant", () => control.SetState("Enter 1"));
+                control.RemoveAction("Enter 1", 5);
+                control.RemoveAction("Enter 1", 3);
+                control.AddCustomAction("Enter 1", () => control.SetState("Activate"));
+                control.RemoveAction("Activate", 3);
+                control.RemoveAction("Activate", 2);
+                control.RemoveAction("Activate", 1);
+                control.RemoveTransition("Set Damage", "FINISHED");
+                control.AddCustomAction("Set Damage", () => control.SetState("Stand"));
+                control.RemoveAction("Stand", 2);
+                control.RemoveAction("Stand", 1);
+                control.RemoveTransition("Stand", "FINISHED");
+                control.RemoveTransition("Stand", "TOOK DAMAGE");
+                control.AddCustomAction("Stand", () => greyPrince.transform.localPosition = new Vector3(0, 2.2788f, 0));
+                foreach (var state in control.FsmStates)
+                {
+                    state.InsertCustomAction(() =>
+                    {
+                        Log("Entering state " + state.Name + ".");
+                    }, 0);
+                }
             }
         }
     }
